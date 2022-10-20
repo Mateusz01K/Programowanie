37291b70ae192b67a3b3cda18dea8fc89ee4047b
Mateusz Kusiak <w65546@student.wsiz.edu.pl>
2022-10-20 12:55:46 +0200

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+using lab2.nowy;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab2.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get(int value)
+        {
+            var calculateInstance = new calculate(value);
+            calculateInstance.Increase(13);
+            return calculateInstance.GetValue();
+        }
+    }
+}
---
+++ nowy/calculate.cs
@@ -0,0 +1,27 @@
+﻿namespace lab2.nowy
+{
+    public class calculate
+    {
+        public calculate(int value)
+        {
+            Value = value;
+        }
+
+        private int Value { get; set; }
+
+        public void Increase(int value)
+        {
+            Value += value;
+        }
+
+        public void Decrease(int value)
+        {
+            Value -= value;
+        }
+
+        public int GetValue()
+        {
+            return Value;
+        }
+    }
+}
